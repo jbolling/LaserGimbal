d39af654a2fdebea8a957cc176991833f5736a94
jbolling <josephcbolling@gmail.com>
2015-05-08 23:07:20 -0400

    working serial receiver

--- GimbalIO/GimbalIO.ino
+++ GimbalIO/GimbalIO.ino
@@ -1,41 +1,46 @@
 #include <Servo.h>
 
+/*
+ *NB - servo center is at 99, 107.
+ */
+
+const int X_SERVO_OFFSET =  9;
+const int Y_SERVO_OFFSET = 17;
+
 Servo x_servo;
 Servo y_servo;
 
 int x_pin = 3;
 int y_pin = 5;
-
-int pos;
+int led_pin = 13;
 
 void setup() {
   x_servo.attach(x_pin);
   y_servo.attach(y_pin);
+  
   Serial.begin(115200);
-  pos = 0;
+  
+  pinMode(led_pin,OUTPUT);
+  digitalWrite(led_pin, LOW);
 }
 
 void loop() {
-  byte buf[3];
-  /*
+  char terminal;
+  long x_pos;
+  long y_pos;
+  
   //update servo values
-  if(Serial.available() >= 3){
-    Serial.readBytes(buf, 3);
+  if(Serial.available() >= 4){
+    x_pos = Serial.parseInt() + X_SERVO_OFFSET;
+    y_pos = Serial.parseInt() + Y_SERVO_OFFSET;
+    terminal = Serial.read();
     
-    if(buf[0] != 0xFF){
+    if(terminal != ';'){
       //packeting error
-      Serial.print("Packeting error");
-    }
-    else{
-      x_servo.write(( buf[1]);
-      y_servo.write((int) buf[2]);
+      digitalWrite(led_pin, HIGH);
+    }else{
+      x_servo.write(x_pos);
+      y_servo.write(y_pos);
     }
   }
-  */
-  
-  x_servo.write(pos);
-  pos++;
-  delay(40);
-  
-  
 }
